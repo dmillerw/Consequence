0fbd9d5bf2dac847e84d85db7c3674e34b94aa00
dmillerw <dmillerw@gmail.com>
2017-01-22 19:50:10 -0800

    Preserve SimpleTable functionality for now

--- src/main/java/me/dmillerw/consequence/lua/javatolua/adapter/SimpleTable.java
+++ src/main/java/me/dmillerw/consequence/lua/javatolua/adapter/SimpleTable.java
@@ -40,7 +40,40 @@ public class SimpleTable extends LuaTable {
 
     /* SETTERS */
 
+    @Override
+    public final void set(int key, LuaValue value) {
+        if (!setValue(String.valueOf(key), value)) super.set(key, value);
+    }
 
+    @Override
+    public final void set(LuaValue key, LuaValue value) {
+        if (!setValue(key.tojstring(), value)) super.set(key, value);
+    }
+
+    @Override
+    public final void set(int key, String value) {
+        if (!setValue(String.valueOf(key), LuaValue.valueOf(value))) super.set(key, value);
+    }
+
+    @Override
+    public final void set(String key, LuaValue value) {
+        if (!setValue(key, value)) super.set(key, value);
+    }
+
+    @Override
+    public final void set(String key, double value) {
+        if (!setValue(key, LuaValue.valueOf(value))) super.set(key, value);
+    }
+
+    @Override
+    public final void set(String key, int value) {
+        if (!setValue(key, LuaValue.valueOf(value))) super.set(key, value);
+    }
+
+    @Override
+    public final void set(String key, String value) {
+        if (!setValue(key, LuaValue.valueOf(value))) super.set(key, value);
+    }
 
     /* END SETTERS */
 }
